398a3cb0e8398e99abd517b111249a42b6db6bb2
maouissaoui <aouissaoui.moufida@gmail.com>
2016-02-26 15:18:34 +0100

    step-1

---
+++ angularmovie-1001/app/js/controllers.js
@@ -0,0 +1,9 @@
+angularMovieApp.controller("homeController" ,function ($scope) {
+    $scope.name = 'Thierry LAU';
+
+});
+
+angularMovieApp.controller("headerController" ,function ($scope) {
+    $scope.name = 'AngularMovie';
+});
+
